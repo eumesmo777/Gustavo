b93c899d7716981550e45967803cbe908b34c7de
eumesmo777 <gustavokubiaki777@gmail.com>
2023-11-30 17:53:47 -0300

    Javascript

---
+++ Estudo JAVA SCRIPT/Matriz/Matriz_Pessoa.js
@@ -0,0 +1,16 @@
+const person = {firstName:"John", lastName:"Doe", age:46};
+const person2 = ["kelly", "silva", 20];
+
+document.getElementById("john").innerHTML = person.firstName+" "+person.lastName+" "+person.age;
+document.getElementById("kelly").innerHTML = person2[0]+" "+person2[1]+" "+person2[2];
+
+//cada tag p deve falar do que falta sobre a matriz
+//person chamada john. ex. escreva o ultimo nome dele e a idade dele
+//apartir da casa 1 e 2 de sua matriz.
+
+
+
+
+
+//como eu acesso a casa 1? person.lastName e no caso da kelly 
+//é person2[1]
