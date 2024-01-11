f4784f2278c9fce24eb3f1ce32a222e04182b586
eumesmo777 <gustavokubiaki777@gmail.com>
2024-01-11 17:32:34 -0300

    Atualizado (11/01/24)

---
+++ Estudo JAVA SCRIPT/Atividade Mari/Funções/Banco.js
@@ -0,0 +1,24 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+cliente1 = new Cliente();
+cliente2 = new Cliente();
+
+cliente1.nome = "Gustavo";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1010;
+cliente1.saldo = 0;
+
+cliente2.nome = "Nathalia";
+cliente2.cpf = 77722233309;
+cliente2.agencia = 1010;
+cliente2.saldo = 0;
+
+cliente3.nome = "Bruna";
+cliente3.cpf = 44422233309;
+cliente3.agencia = 1010;
+cliente3.saldo = 0;
